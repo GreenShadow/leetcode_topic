09332d10789167b94d7dbcae61dd4b830719a857
Guo Ruyi <guoruyi@smartisan.com>
2018-09-07 21:07:33 +0800

    Second day

---
+++ src/day2/topic1_length_of_longest_substring/Best.java
@@ -0,0 +1,15 @@
+package day2.topic1_length_of_longest_substring;
+
+public class Best {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(i)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(i)] = j + 1;
+        }
+        return ans;
+    }
+}
